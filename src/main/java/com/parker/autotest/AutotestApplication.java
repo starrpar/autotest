487f5ce8df1978c3df9012c0500f9427ed85d245
starrpar <starrparker@live.com>
2018-11-03 17:35:20 -0700

    initial commit

---
+++ src/main/java/com/parker/autotest/AutotestApplication.java
@@ -0,0 +1,12 @@
+package com.parker.autotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutotestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutotestApplication.class, args);
+	}
+}
